6ab7029ad2473412cdcfdc6769750e6e027a1bf2
Nickp445 <napresler@gmail.com>
2022-05-19 00:16:50 -0400

    World Added

--- src/world/worlddata.ts
+++ src/world/worlddata.ts
@@ -5,9 +5,9 @@ house:
         src: '../worldscenes/CharHouse.png',
         sceneName: "My House",
         textNode: "You wake up with a busy day ahead of you! I think today ill go to...",
-        option1: "Go to the church and speak with father Simon.",
+        option1: "Go to the church and speak with Father Simon.",
         option2: "Go to the castle and speak with The King.",
-        option3: "Go to the citadel and speak with The keep of the books.",
+        option3: "Go to the citadel and speak with The Keeper of the Books.",
         option4: "Go to the market and get something to eat for the day."
     }
 }
